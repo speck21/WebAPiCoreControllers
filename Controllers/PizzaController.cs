2999e85c2e56d944a1a2d4b80a80c287fae7c806
Speck <stephan.speck21@gmail.com>
2024-03-07 21:27:14 -0600

    pizza controller

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,15 @@
+using ContosoPizza.Models;
+using ContosoPizza.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContosoPizza.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PizzaController : ControllerBase
+{
+    public PizzaController()
+    {
+        
+    }
+}
